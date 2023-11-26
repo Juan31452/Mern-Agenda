c8d9d87acead0241a19ddf26f61f18535a41dd67
Juan Carlos <jcs1011@gmail.com>
2023-11-26 14:02:25 +0100

    se crean rutas carpetas components,context,pages,archivos login y register

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,22 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+     <Routes>
+      <Route path='/' element={<h1>Home Page</h1>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/logout' element={<Register/>} />
+      <Route path='/perfil' element={<h1>Perfil</h1>}/>
+      <Route path='/register' element={<h1>Register</h1>}/>
+      <Route path='/adddiary' element={<h1>Add Diary</h1>}/>
+      <Route path='/diary' element={<h1>Diary</h1>}/>
+      <Route path='/deletediary' element={<h1>Delete Diary</h1>}/> 
+      <Route path='/updatediary' element={<h1>Update Diary</h1>}/>
+     </Routes>
+    </BrowserRouter>
   )
 }
 
